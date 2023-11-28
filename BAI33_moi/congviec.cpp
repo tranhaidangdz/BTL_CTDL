1ccccc7f078e6da5ba32cb524c6d0114d0e68de2
tranhdaidangdz <trandang211@gmail.com>
2023-11-28 21:49:32 +0700

    sua lai

--- BAI33_moi/congviec.cpp
+++ BAI33_moi/congviec.cpp
@@ -25,9 +25,74 @@ public:
     {
         return Ten;
     }
+#ifndef CONG_VIEC_CPP
+#define CONG_VIEC_CPP
+#include <iostream>
+#include <string>
+#include <iomanip>
+        using namespace std;
+
+    class CongViec
+    {
+    protected:
+        int Gio;
+        string Ten;
+        string NoiDung;
+        string TinhChat;
+
+    public:
+        CongViec(int Gio = 0, string Ten = "", string NoiDung = "", string TinhChat = "");
+        friend istream &operator>>(istream &in, CongViec &a);
+        friend ostream &operator<<(ostream &out, CongViec a);
+        int getGio()
+        {
+            return Gio;
+        }
+        string getTen()
+        {
+            return Ten;
+        }
+        string getTinhChat()
+        {
+            return TinhChat;
+        }
+    };
+
+    CongViec::CongViec(int Gio, string Ten, string NoiDung, string TinhChat)
+    {
+        this->Gio = Gio;
+        this->Ten = Ten;
+        this->NoiDung = NoiDung;
+        this->TinhChat = TinhChat;
+    }
+    istream &operator>>(istream &in, CongViec &a)
+    {
+        cout << "Gio bat dau:";
+        in >> a.Gio;
+        in.ignore();
+        cout << "Ten cong viec:";
+        getline(in, a.Ten);
+        cout << "Noi dung:";
+        getline(in, a.NoiDung);
+        cout << "Tinh chat:";
+        getline(in, a.TinhChat);
+        return in;
+    }
+    ostream &operator<<(ostream &out, CongViec a)
+    {
+        out << setw(6) << "Gio bat dau:" << setw(20) << "Ten cong viec:" << setw(22) << "Noi dung:" << setw(18) << "Tinh chat:\n";
+        out << setw(6) << a.Gio << setw(20) << a.Ten << setw(22) << a.NoiDung << setw(18) << a.TinhChat;
+        return out;
+    }
+#endif
     string getTinhChat()
     {
         return TinhChat;
+    }
+
+    void setTinhChat(string TinhChat)
+    {
+        this->TinhChat = TinhChat;
     }
 };
 
--- BAI33_moi/main.cpp
+++ BAI33_moi/main.cpp
@@ -13,11 +13,11 @@ int main()
 {
     QuanLy a; // luu ý khi khai báo biến có kdl class bị lỗi ta phải xem lại trong class đó có hàm tạo ko đối chưa?
     a.Nhap_ND_CongViec_TheoNgay_TheoGio();
-    // a.Xem_Lich_CongTac_TheoNgay();
-    // a.Xem_CongViec_Theo_TinhChat();
-    // a.Xem_CongViec_Da_Hoan_Tat();
-    // a.Xem_CongViec_Chua_Hoan_Tat();
-    // a.Xem_CongViec_Tu_NgayA_den_NgayB();
+    a.Xem_Lich_CongTac_TheoNgay();
+    a.Xem_CongViec_Theo_TinhChat();
+    a.Xem_CongViec_Da_Hoan_Tat();
+    a.Xem_CongViec_Chua_Hoan_Tat();
+    a.Xem_CongViec_Tu_NgayA_den_NgayB();
     a.DIeu_Chinh_Lich_Cong_Tac();
     return 0;
 }
